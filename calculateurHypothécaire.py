5067a09078e92f75f930b45237a76a7b122ee14c
Arthur-Olivier Fortin <aofor@ulaval.ca>
2023-03-23 21:03:53 -0400

    modif

--- calculateurHypothécaire.py
+++ calculateurHypothécaire.py
@@ -1,32 +1,60 @@
 
 
 def calculateurIntérêt():
-    PrixLogement = input("Entré le prix du logement (sans le signe de dollars)\n")
-    MiseDeFond = input("Entré la mise de fond (sans le signe de dollars)\n")
-    Hypothèque=(PrixLogement-MiseDeFond)
-    print(f"Hypothèque = {Hypothèque}\n")
-    intérêts = input("Entré le pourcentage d'intérêts (sans le signe de %)\n")
-    ammortissement = input("Entrez le nombre d'années d'ammortissement\n")
-    hydro = input("Entrez les frais annuels d'hydro (sans le signee de dollars)\n")
-    taxes = input("Entrez les frais annuels de taxes (sans le signe de dollars)")
-    autres = input('''Entrez les frais annuels "autres" (sans le signee de dollars)\n''')
+    
+    PrixLogement = float(214000)#float(input("Entré le prix du logement (sans le signe de dollars)\n"))
+    MiseDeFond = float(75000)#float(input("Entré la mise de fond (sans le signe de dollars)\n"))
+    Hypothèque=(PrixLogement)-MiseDeFond
+    intérêts = float(5.89)#float(input("Entré le pourcentage d'intérêts (sans le signe de %)\n"))
+    intérêts /= 10
+    ammortissement = int(25)#int(input("Entrez le nombre d'années d'ammortissement\n"))
+    hydro = float(804)#float(input("Entrez les frais annuels d'hydro (sans le signee de dollars)\n"))
+    taxes = float(2012)#float(input("Entrez les frais annuels de taxes (sans le signe de dollars)\n"))
+    autres = float(1545)#float(input('''Entrez les frais annuels "autres" (sans le signee de dollars)\n'''))
+    fraisAnnuelsHypothèque = (Hypothèque/25)
+    FraisTotaux = (Hypothèque+(+hydro+taxes+autres)*ammortissement)
+
+    print(f"............\n")
+    print(f"Hypothèque = {Hypothèque}$\n")
+    print(f"intérêt = {intérêts}$\n")
+    print(f"ammortissement = {ammortissement}$\n")
+    print(f"hydro = {hydro}$\n")
+    print(f"taxes = {taxes}$\n")
+    print(f"autres = {autres}$\n")
+    
+
     
     sommeTotale = 0
-    fraisMensuel = (((Hypothèque/ammortissement)+hydro+taxes+autres)/12)
+    fraisAnnuels = (((Hypothèque/ammortissement)+hydro+taxes+autres))
     intérêtsTotale = 0
 
-    
-    for i in range(ammortissement-1):
-        print(f"====== Année {i+1} ======\n")
-        intérêtsAnnuels = (Hypothèque*intérêts)
-        sommeTotale+=((fraisMensuel*12)+intérêtsAnnuels)
-        Hypothèque-=(Hypothèque-(fraisMensuel*12))
-        intérêtsTotale += intérêtsAnnuels
-        print(f"Les intérêts payés lors de la {i+1} années = {intérêtsAnnuels}")
-        print(f"Les frais totaux lors de la {i+1} années = {(fraisMensuel*12)+intérêtsAnnuels}")
+    print(f"Frais Annuels (sans les intérêts)  = {fraisAnnuels}$\n")
+    print(f"Frais Mensuels (sans les intérêts)  = {fraisAnnuels/12}$\n")
+    print(f".............\n")
 
+    
+    for i in range(ammortissement):
+        print(f"====== Année {i+1} ======")
+        intérêtsAnnuels = (Hypothèque*intérêts)/(ammortissement)
+        print(f" IntérêtsAnnuels = {intérêtsAnnuels}%\n")
+        sommeTotale+=((fraisAnnuels)+intérêtsAnnuels)
+        print(f" sommeTotale payé de l'hypothèque à date = {sommeTotale}$\n")
+        FraisTotaux-=(fraisAnnuels)
+        intérêtsTotale += intérêtsAnnuels
+        Hypothèque-=(fraisAnnuelsHypothèque)
+        print(f"Somme des intérêts payé à date = {intérêtsTotale}$")
+        print(f"Les intérêts payés lors de l'année {i+1} = {intérêtsAnnuels}$")
+        print(f"Les frais totaux lors de l'année {i+1} = {(fraisAnnuels)+intérêtsAnnuels}$")
+        print(f"Frais restant à payer ((Hypothèque+intérêts Totaux) - montant payé) = {FraisTotaux}$")
+
+    print(" ======== Résultats : ")
+    print(f"Pour un immeuble de {PrixLogement}$ avec une mise de fond de {MiseDeFond}:\n")
+    print("===========")
+    print(f"L'hypothèque sera de {Hypothèque}$\n")
     print(f"somme totale à rembourser (intérêt + Hypothèque) = {sommeTotale}\n")
     print("===========")
-    print(f"somme totale des intérêts sur {ammortissement} d'ammortissement à rembourser = {intérêtsTotale}\n")
+    print(f"somme totale des intérêts sur {ammortissement} ans d'ammortissement à rembourser = {intérêtsTotale}\n")
     print("===========")
+
+calculateurIntérêt()
 
